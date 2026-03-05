f977597caf0fdb723b897d0a4be358dbf76e9dac
Varun Agarwal <varunagarwal@mac.lan>
2026-03-05 02:09:28 -0700

    postgres

--- server/backend/backend/settings.py
+++ server/backend/backend/settings.py
@@ -138,23 +138,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 # Set default values for the environment variables if they’re not already set
-os.environ.setdefault("PGDATABASE", "tsa")
-os.environ.setdefault("PGUSER", "myuser")
-os.environ.setdefault("PGPASSWORD", "mypassword")
-os.environ.setdefault("PGHOST", "localhost")
-os.environ.setdefault("PGPORT", "5432")
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': int(os.environ["PGPORT"]),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PGDATABASE", "railway"),  # default to Railway DB
+        "USER": os.environ.get("PGUSER", "postgres"),
+        "PASSWORD": os.environ.get("PGPASSWORD", "oFGrdmVIiyzvmJUsjTyHgfQxRDcoWQMG"),
+        "HOST": os.environ.get("PGHOST", "postgres.railway.internal"),
+        "PORT": os.environ.get("PGPORT", "5432"),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
